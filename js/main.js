5403d0d7c5cf875edef4ab3f0aafb96967d2c750
panchenko-smilebz <panchenkovitalij@gmail.com>
2022-04-04 06:02:32 +0300

    changed file structure

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ let bgVideoWebm = document.querySelector('#bgVideoWebm');
 let bgVideoMp4 = document.querySelector('#bgVideoMp4');
 
 if ( window.screen.width <= 930 ) {
-    bgVideoPoster.setAttribute('poster', 'video/pexels-photo-10622156.webp'),
-    bgVideoWebm.setAttribute('src', 'video/pexels-ivan-photo-10622156.webm'),
-    bgVideoMp4.setAttribute('src', 'video/pexels-ivan-photo-10622156.mp4')
+    bgVideoPoster.setAttribute('poster', 'assets/video/pexels-photo-10622156.webp'),
+    bgVideoWebm.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.webm'),
+    bgVideoMp4.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.mp4')
 } else if ( window.screen.width >= 931 ) {
-    bgVideoPoster.setAttribute('poster', 'video/NOMI_video backplate_v2-poster.jpg'),
-    bgVideoWebm.setAttribute('src', 'video/NOMI_video backplate_v2.webm'),
-    bgVideoMp4.setAttribute('src', 'video/NOMI_video backplate_v2.mp4')
+    bgVideoPoster.setAttribute('poster', 'assets/video/NOMI_video backplate_v2-poster.jpg'),
+    bgVideoWebm.setAttribute('src', 'assets/video/NOMI_video backplate_v2.webm'),
+    bgVideoMp4.setAttribute('src', 'assets/video/NOMI_video backplate_v2.mp4')
 }
