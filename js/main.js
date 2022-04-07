89c96c49d38381c6be9eb199b17f8699f9029498
panchenko-smilebz <panchenkovitalij@gmail.com>
2022-04-07 20:03:42 +0300

    made all leyouts for final changes and launch

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 'use strict';
 
-// Horisontal and vertical BG video change
-let bgVideoPoster = document.querySelector('#bgVideo');
-let bgVideoWebm = document.querySelector('#bgVideoWebm');
-let bgVideoMp4 = document.querySelector('#bgVideoMp4');
+// // Horisontal and vertical BG video change
+// let bgVideoPoster = document.querySelector('#bgVideo');
+// let bgVideoWebm = document.querySelector('#bgVideoWebm');
+// let bgVideoMp4 = document.querySelector('#bgVideoMp4');
 
-if ( window.screen.width <= 930 ) {
-    bgVideoPoster.setAttribute('poster', 'assets/video/pexels-photo-10622156.webp');
-    bgVideoWebm.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.webm');
-    bgVideoMp4.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.mp4');
-} else if ( window.screen.width >= 931 ) {
-    bgVideoPoster.setAttribute('poster', 'assets/video/NOMI_video backplate_v2-poster.jpg');
-    bgVideoWebm.setAttribute('src', 'assets/video/NOMI_video backplate_v2.webm');
-    bgVideoMp4.setAttribute('src', 'assets/video/NOMI_video backplate_v2.mp4');
-}
+// if ( window.screen.width <= 930 ) {
+//     bgVideoPoster.setAttribute('poster', 'assets/video/pexels-photo-10622156.webp');
+//     bgVideoWebm.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.webm');
+//     bgVideoMp4.setAttribute('src', 'assets/video/pexels-ivan-photo-10622156.mp4');
+// } else if ( window.screen.width >= 931 ) {
+//     bgVideoPoster.setAttribute('poster', 'assets/video/NOMI_video backplate_v2-poster.jpg');
+//     bgVideoWebm.setAttribute('src', 'assets/video/NOMI_video backplate_v2.webm');
+//     bgVideoMp4.setAttribute('src', 'assets/video/NOMI_video backplate_v2.mp4');
+// }
 
 // Colcade masonry layout for portfolio section
 var colc = new Colcade( '.grid', {
@@ -41,5 +41,14 @@ window.onload = ()=>{
                 }
             });
         }
-    }
-}
+    };
+};
+
+// FAQ accordion
+const faqs = document.querySelectorAll('.questions__item');
+
+faqs.forEach(questions__item => {
+    questions__item.addEventListener('click', () => {
+        questions__item.classList.toggle('active');
+    });
+});
